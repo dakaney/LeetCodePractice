887ce3eb041fcf68ba92286165caf67af52e4e19
Dakaney <wayman_ng@yahoo.com>
2019-03-05 12:34:35 -0800

    completed 575

--- DayTwo/575DistributeCandies.js
+++ DayTwo/575DistributeCandies.js
@@ -1,11 +1,24 @@
+// var distributeCandies = function(candies) {
+//     let uniqueCandies = 0;
+//     let candyTypes = {};
+//     for (let i = 0; i < candies.length; i++) {
+//         if (!candyTypes[candies[i]]) {
+//             uniqueCandies += 1;
+//             candyTypes[candies[i]] = true;
+//         }
+//     }
+//     return candies.length >= uniqueCandies * 2 ? uniqueCandies : Math.min(candies.length / 2, uniqueCandies);
+// };
+
+
 var distributeCandies = function(candies) {
-    let uniqueCandies = 0;
-    let candyTypes = {};
+    let candyTypes = new Set();
     for (let i = 0; i < candies.length; i++) {
-        if (!candyTypes[candies[i]]) {
-            uniqueCandies += 1;
-            candyTypes[candies[i]] = true;
+        if (candyTypes.size < candies.length / 2) {
+            candyTypes.add(candies[i])
+        } else {
+            break;
         }
     }
-    return candies.length >= uniqueCandies * 2 ? uniqueCandies : Math.min(candies.length / 2, uniqueCandies);
+    return candyTypes.size;
 };
