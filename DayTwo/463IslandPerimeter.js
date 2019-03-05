2d4b9dac2fb20a69f3b7768d29794d5f5aa7e179
Dakaney <wayman_ng@yahoo.com>
2019-03-05 10:43:39 -0800

    completed 463

---
+++ DayTwo/463IslandPerimeter.js
@@ -0,0 +1,14 @@
+var islandPerimeter = function(grid) {
+    let count = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 1) {
+                if (!grid[i-1] || grid[i-1][j] !== 1) count += 1;
+                if (!grid[i+1] || grid[i+1][j] !== 1) count += 1;
+                if (grid[i][j-1] !== 1) count += 1;
+                if (grid[i][j+1] !== 1) count += 1;
+            }
+        }
+    }
+    return count;
+};
