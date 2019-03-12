7565fd723d9f0813f2dddec0b8b2cf8d9ec51f02
Dakaney <wayman_ng@yahoo.com>
2019-03-11 22:27:08 -0700

    completed 448

---
+++ DaySix/448FindMissingNumbers.js
@@ -0,0 +1,11 @@
+var findDisappearedNumbers = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+        nums[index] = Math.abs(nums[index]) * -1;
+    }
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) result.push(i + 1)
+    }
+    return result;
+};
